3215e7133be1eb758132b56f32e1a0b5fda9b961
Johan <johan@macair2020>
2020-06-13 22:06:23 +0200

    publish

---
+++ docs/precache-manifest.a932f80c9ab3c3c77da63c91ddf47af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6393ce0e895dc888924c0ee25e5a7ce1",
+    "url": "/ws-bitstamp/index.html"
+  },
+  {
+    "revision": "3b2a30062fe04bc0e0c4",
+    "url": "/ws-bitstamp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "69f01767d583a5794808",
+    "url": "/ws-bitstamp/static/js/2.5424300d.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/ws-bitstamp/static/js/2.5424300d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2a30062fe04bc0e0c4",
+    "url": "/ws-bitstamp/static/js/main.866946db.chunk.js"
+  },
+  {
+    "revision": "0174cf79180f6cdcd715",
+    "url": "/ws-bitstamp/static/js/runtime-main.b111871a.js"
+  }
+]);
