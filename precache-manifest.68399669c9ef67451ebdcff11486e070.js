2d50a9b3032bb9f089afd9a627823d0f0dcfe32b
johnb8005 <johnb8005@users.noreply.github.com>
2022-04-16 11:22:04 +0000

    deploy: 3950e4ca9cb6f1a710002f11ef857c081912f5f2

---
+++ precache-manifest.68399669c9ef67451ebdcff11486e070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c3503171000f204dee5dad6139413c",
+    "url": "/ws-bitstamp/index.html"
+  },
+  {
+    "revision": "64ae0bcdedc278099e66",
+    "url": "/ws-bitstamp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fd3bb9b303c105262961",
+    "url": "/ws-bitstamp/static/js/2.b3036b77.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/ws-bitstamp/static/js/2.b3036b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ae0bcdedc278099e66",
+    "url": "/ws-bitstamp/static/js/main.f78ecd8c.chunk.js"
+  },
+  {
+    "revision": "0174cf79180f6cdcd715",
+    "url": "/ws-bitstamp/static/js/runtime-main.b111871a.js"
+  }
+]);
